5485fd982288424921cfc9317220a7159903a7fe
AlexVRmin <159445374+AlexVRmin@users.noreply.github.com>
2024-03-19 10:03:59 +0100

    Add files via upload

--- dashboard/src/routes/+page.server.js
+++ dashboard/src/routes/+page.server.js
@@ -2,8 +2,10 @@ import { supabase } from "$lib/supabaseClient";
 
 /**Hier zal je de naam_tabel veranderen met de naam van de tabel die je wilt hiermee connecteren */
 export async function load() {
-  const { data } = await supabase.from("naam_tabel").select();
+  const { data, count } = await supabase.from("School").select(`*`, { count: 'exact'});
   return {
-    naam_tabel: data ?? [],
+    School: data ?? [],
+    Count: count ?? [],
   };
 }
+
--- dashboard/tailwind.config.js
+++ dashboard/tailwind.config.js
@@ -2,7 +2,12 @@
 export default {
   content: ['./src/**/*.{html,js,svelte,ts}'],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        "edutech-orange": "#F29005"
+      },
+
+    },
   },
   plugins: [],
 }
