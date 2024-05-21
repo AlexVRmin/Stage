cc6efdd86a424eb2339b5d6c3ffbfda6021b3c71
AlexVRmin <159445374+AlexVRmin@users.noreply.github.com>
2024-05-21 15:14:08 +0200

    Delete dashboard directory

--- dashboard/src/lib/supabaseClient.js
+++
@@ -1,3 +0,0 @@
-import { createClient } from '@supabase/supabase-js'
-
-export const supabase = createClient('https://facbywqwkyedmxbkgzpr.supabase.co', 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImZhY2J5d3F3a3llZG14YmtnenByIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MDI1NDM0NzYsImV4cCI6MjAxODExOTQ3Nn0.Usho0XM_c36DBYMsK6EPO0XtLU-ckCtiUL-aG1_wKhA')
