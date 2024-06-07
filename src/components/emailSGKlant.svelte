32b87c0e2d6d4f9c9170057cdeb9f06b8183726e
AlexVRmin <159445374+AlexVRmin@users.noreply.github.com>
2024-06-07 09:34:10 +0200

    Add files via upload

--- src/components/emailSGKlant.svelte
+++ src/components/emailSGKlant.svelte
@@ -15,17 +15,14 @@
 
 <div>
     <Text>
-        <div style="width: 100%; background-color: gray; display: flex; justify-content: center;">
-            <div style="display: flex; width: 40%; min-width: 500px; background-color: black; color: #fff; flex-direction: column;">
-                <img src="https://edu-tech.be/wp-content/uploads/2020/11/Logo-only-small-e1606142482154.png" alt="Logo van edu-tech">
+        <div style="width: 100%; background-color: #666666; display: flex; justify-content: center;">
+            <div style="display: flex; width: 40%; min-width: 500px; background-color: gray; color: #fff; flex-direction: column;">
+                <img src="https://edu-tech.be/wp-content/uploads/2020/11/Logo-only-small-e1606142482154.png" style="height: 10%; width: 10%; margin: 25px auto 0px auto;" alt="Logo van edu-tech">
                 <br><br>
                 <h1 style="text-align: center; color: #fff;">Edu-Tech BV</h1>
-                <h1 style="text-align: center; color: #fff;">Er is een aankoop gemaakt!</h1>
-                <p style="text-align: center; color: #fff;">{client} heeft (een) licentie(s) gekocht.</p>
-                <a href="https://edu-tech.be/" target="_blank" style="color: black; text-align: center; text-decoration: none; margin: 5px 30%; padding: 7px 13px; background-color: rgb(243, 145, 3); border-radius: 5%;">Bekijk De Licentie(s)</a>
-                <h3 style="text-align: center; color: #fff;">Wil je de aankoop accepteren?</h3>
-                <a href="https://edu-tech.be/" target="_blank" style="color: black; text-align: center; text-decoration: none; margin: 5px 30%; padding: 7px 13px; background-color: rgb(243, 145, 3); border-radius: 5%;">Ja</a>
-                <a href="https://edu-tech.be/" target="_blank" style="color: black; text-align: center; text-decoration: none; margin: 5px 30%; padding: 7px 13px; background-color: rgb(243, 145, 3); border-radius: 5%;">Nee</a>
+                <h1 style="text-align: center; color: #fff;">Dank u voor uw aankoop!</h1>
+                <p style="text-align: center; color: #fff;">Hallo, {client}</p>
+                <p style="color: #fff; text-align: center;">Bekijk deze gegevens grondig na. Is er iets fout? Stuur een mail naar <a href="#" style="color: blue;">info@edu-tech.be</a>!!</p>
                 <hr style="border: 1px solid rgb(243, 145, 3); width: 100%;">
                 <h2 style="padding-left: 15px;">Klant Informatie:</h2>
                 <hr style="border-top: 1px solid rgb(243, 145, 3); width: 100%;">
@@ -47,7 +44,7 @@
                 <h2 style="color: white; padding-left: 15px;">Bestelling:</h2>
                 <hr style="border-top: 1px solid rgb(243, 145, 3); width: 100%;">
                 <ul style="display: flex; flex-direction: row; gap: 20px 10px;">
-                    <li style="color: #fff; width: 250px; white-space: pre-line; list-style-type: none; padding: 5px;">{licentie}</li>
+                    <li style="color: #fff; width: 250px; list-style-type: none; padding: 5px;">{licentie}</li>
                     <li style="color: #fff; width: 250px; list-style-type: none; padding: 5px;">{amount}x</li>
                 </ul>
                 <br><br>
