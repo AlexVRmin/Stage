818a888db59eff63fc3ea2125450a8b91b0ee43b
AlexVRmin <159445374+AlexVRmin@users.noreply.github.com>
2024-06-10 09:43:57 +0200

    Add Footer

---
+++ src/components/Footer.svelte
@@ -0,0 +1,20 @@
+<footer class="bg-black text-white mt-20">
+    
+    <div class="flex items-center justify-center flex-col pt-5 pb-10 gap-3">
+        
+        <div class="flex items-center gap-2">
+            <img src="src/lib/Images/Logo.png" alt="Logo">
+            <h3 class="text-edutech-orange font-semibold text-2xl">EDU-TECH</h3>    
+        </div>
+
+        <p>Uw toegewijde onderwijspartner gedreven door passie voor het onderwijs en een sterke behoefte aan IT-integratie en cloudbeheer.</p>
+
+        <ul class="flex gap-2">
+            <li><a class="text-2xl" href="https://www.facebook.com/EDUTechBE" target="_blank"><i class="fa-brands fa-facebook" style="color: #f29005;"></i></a></li>
+            <li><a class="text-2xl" href="#"><i class="fa-brands fa-youtube" style="color: #f29005;"></i></a></li>
+            <li><a class="text-2xl" href="https://www.linkedin.com/company/18832527/admin/feed/posts/" target="_blank"><i class="fa-brands fa-linkedin" style="color: #f29005;"></i></a></li>
+            <li><a class="text-2xl" href="https://github.com/Edu-Tech-Group" target="_blank"><i class="fa-brands fa-github" style="color: #f29005;"></i></a></li>
+        </ul>
+    </div>
+
+</footer>
